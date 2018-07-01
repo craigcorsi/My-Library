0d7ee524b7796f146f114026c96c281ec61acc67
Craig <corsi007@umn.edu>
2018-07-01 11:24:49 -0500

    spi routes with parameter

--- server.js
+++ server.js
@@ -3,18 +3,21 @@ var express = require("express");
 var app = express();
 var PORT = 3000;
 
-var notes = [
+var works = [
     {
+        keyword: 'didion1',
         title: 'The Year of Magical Thinking',
         author: 'Joan Didion',
         genre: 'memoir',
         notes: 'There was a scene I liked but forgot; I think she was writing her late husband letters and to-do lists even though he had died...',
     }, {
+        keyword: 'geometry1',
         title: 'A Mathematical Space Odyssey: Solid Geometry in the 21st Century',
         author: 'Claudi Alsina and Roger Nelson',
         genre: 'mathematics',
         notes: 'I loved being able to show an 8th-grader the visual proofs of summation formulas.',
     }, {
+        keyword: 'anshaw1',
         title: 'The Last Speaker of the Language',
         author: 'Carol Anshaw',
         genre: 'short fiction',
@@ -24,25 +27,28 @@ var notes = [
 
 
 
-app.get("/", function(req, res){
-    res.send("Welcome to my library! Try routes /didion, /geometry, or /anshaw!");
+app.get("/", function (req, res) {
+    res.send("Welcome to my library! Try routes /api/works followed by /didion1, /geometry1, or /anshaw1!");
 });
 
-app.get("/didion", function(req, res){
-    res.json(notes[0]);
+app.get("/api/works", function (req, res) {
+    return res.json(works);
 });
 
-app.get("/geometry", function(req, res){
-    res.json(notes[1]);
+app.get("/api/works/:work", function (req, res) {
+    var chosen = req.params.work;
+    console.log(chosen);
+
+    for (var i = 0; i < works.length; i++) {
+        if (chosen === works[i].keyword) {
+            return res.json(works[i]);
+        }
+    }
+    return res.send("No entry found!");
 });
 
-app.get("/anshaw", function(req, res){
-    res.json(notes[2]);
-});
 
 
-
-
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
